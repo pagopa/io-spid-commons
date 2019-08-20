ee92875a149994a49950e12aa32f645c5c59bbde
Daniele Manni <dan.manni@gmail.com>
2019-08-20 16:25:12 +0200

    [#167976925] add type def exports

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="./@types/spid-passport/index.d.ts"/>
+import * as SpidPassport from "spid-passport";
+
+/// <reference path="./@types/x509/index.d.ts"/>
+import * as x509 from "x509";
+
+export { SpidPassport, x509 };
