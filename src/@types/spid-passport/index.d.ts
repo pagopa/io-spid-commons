34299202131bdce425ce785399b1d2d3d0d65af0
alexgpeppe <alexgpeppe84@hotmail.it>
2019-08-21 09:10:21 +0200

    [167976925] Fix lint errors in spid-passport types definition file

--- src/@types/spid-passport/index.d.ts
+++ src/@types/spid-passport/index.d.ts
@@ -2,7 +2,7 @@
  * This files contains the typescript declaration of module spid-passport.
  */
 
-declare module "spid-passport"
+declare module "spid-passport";
 
 interface IDPOption {
   // tslint:disable-next-line: readonly-array
@@ -19,16 +19,16 @@ interface ISpidStrategyOptions {
 }
 
 declare class SpidStrategy<T> {
-  constructor(config: ISpidStrategyOptions, _: (
-    profile: T,
-    done: (err: Error | undefined, info: T) => void
-  ) => void );
-  // tslint:disable-next-line:no-any
-  public logout(req: any, callback?: (err: any, request: any) => void): void;
-  public generateServiceProviderMetadata(samlCert: string): string;
   public spidOptions: {
     idp: { [key: string]: IDPOption | undefined };
     // tslint:disable-next-line: no-any
     sp: any;
   };
+  constructor(
+    config: ISpidStrategyOptions,
+    _: (profile: T, done: (err: Error | undefined, info: T) => void) => void
+  );
+  // tslint:disable-next-line:no-any
+  public logout(req: any, callback?: (err: any, request: any) => void): void;
+  public generateServiceProviderMetadata(samlCert: string): string;
 }
