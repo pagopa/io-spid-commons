51649b11f17e0eb51376c1671b20aeaba6947bce
alexgpeppe <alexgpeppe84@hotmail.it>
2019-08-21 15:36:01 +0200

    [167976925] Remove type type def exports from src folder

--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="./@types/spid-passport/index.d.ts"/>
-import * as SpidPassport from "spid-passport";
-
-/// <reference path="./@types/x509/index.d.ts"/>
-import * as x509 from "x509";
-
-export { SpidPassport, x509 };
