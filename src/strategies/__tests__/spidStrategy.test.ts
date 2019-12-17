837a7972d136ac12dbfb993cb439d4d43f4c663f
alexgpeppe <alexgpeppe84@hotmail.it>
2019-12-17 17:38:13 +0100

    [#170334278] Add test suite for loadFromRemote method

---
+++ src/strategies/__tests__/spidStrategy.test.ts
@@ -0,0 +1,22 @@
+import { IDP_IDS, loadFromRemote } from "../spidStrategy";
+
+describe("loadFromRemote", () => {
+  it("should reject if the fetch of IdP metadata fails", () => {
+    const notExistingUrl = "http://0.1.2.3/index.html";
+    const result = loadFromRemote(notExistingUrl, IDP_IDS);
+    return expect(result).rejects.toEqual(expect.any(Error));
+  });
+
+  it("should reject an error if the fetch of IdP metadata returns no useful data", () => {
+    const wrongIdpMetadataUrl = "http://www.example.com";
+    const result = loadFromRemote(wrongIdpMetadataUrl, IDP_IDS);
+    return expect(result).rejects.toEqual(expect.any(Error));
+  });
+
+  it("should resolve with the fetched IdP options", () => {
+    const validIdpMetadataUrl =
+      "https://registry.spid.gov.it/metadata/idp/spid-entities-idps.xml";
+    const result = loadFromRemote(validIdpMetadataUrl, IDP_IDS);
+    return expect(result).resolves.toBeTruthy();
+  });
+});
