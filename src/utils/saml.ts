c3e7e8592febefde715989c94c7864c18cfaa347
Daniele Manni <dan.manni@gmail.com>
2020-02-10 11:25:08 +0100

    Add validation and split in smaller functions

--- src/utils/saml.ts
+++ src/utils/saml.ts
@@ -470,6 +470,272 @@ export const getAuthorizeRequestTamperer = (
 //  Validate response
 //
 
+const utcStringToDate = (value: string, tag: string): Date => {
+  const maybeDate =
+    value.length === 24
+      ? UTCISODateFromString.decode(value)
+      : UTCISODateFromString.decode(value.replace("Z", ".000Z"));
+  if (isLeft(maybeDate)) {
+    throw new Error(`${tag} must be an UTCISO format date string`);
+  }
+  return maybeDate.value;
+};
+
+const validateIssuer = (fatherElement: Element) => {
+  const IssuerElement = fatherElement
+    .getElementsByTagNameNS(SAML_NAMESPACE.ASSERTION, "Issuer")
+    .item(0);
+  if (
+    !IssuerElement ||
+    !IssuerElement.textContent ||
+    IssuerElement.textContent === ""
+  ) {
+    throw new Error("Issuer element must be present and not empty");
+  }
+  // TODO: Must validate that Issuer value is equal to EntityID IDP (29)(69)
+  /*if (IssuerElement.textContent !== samlConfig.idpIssuer) {
+    throw new Error(`"Invalid Issuer value: "${samlConfig.idpIssuer}`);
+  }*/
+  const IssuerFormatValue = IssuerElement.getAttribute("Format");
+  if (!IssuerFormatValue) {
+    throw new Error("Format attribute of Issuer element must be present");
+  }
+  if (
+    IssuerFormatValue !== "urn:oasis:names:tc:SAML:2.0:nameid-format:entity"
+  ) {
+    throw new Error("Format attribute of Issuer element is invalid");
+  }
+};
+
+const mainAttributeValidation = (requestOrAssertion: Element) => {
+  const ID = requestOrAssertion.getAttribute("ID");
+  if (!ID && ID === "") {
+    throw new Error("Assertion must contain a non empty ID");
+  }
+  const Version = requestOrAssertion.getAttribute("Version");
+  if (Version !== "2.0") {
+    throw new Error("Version version must be 2.0");
+  }
+  const IssueInstant = requestOrAssertion.getAttribute("IssueInstant");
+  if (!IssueInstant) {
+    throw new Error("Assertion must contain a non empty IssueInstant");
+  }
+  const IssueInstantDate = utcStringToDate(IssueInstant, "IssueInstant");
+  if (IssueInstantDate.getTime() > Date.now()) {
+    throw new Error("IssueInstant must be in the past");
+  }
+  return IssueInstantDate;
+};
+
+const isEmptyNode = (element: Element): boolean => {
+  if (element.childNodes.length > 1) {
+    return false;
+  } else if (
+    element.firstChild &&
+    element.firstChild.nodeType === element.ELEMENT_NODE
+  ) {
+    return false;
+  } else if (
+    element.textContent &&
+    element.textContent
+      ?.split("\r") // Exclude return character
+      .join("")
+      .split("\n") // Exclude new line character
+      .join("")
+      .split(" ") // Exclude spaces
+      .join("") !== ""
+  ) {
+    return false;
+  }
+  return true;
+};
+
+const notOnOrAfterValidation = (element: Element) => {
+  const notOnOrAfter = element.getAttribute("NotOnOrAfter");
+  if (!notOnOrAfter || notOnOrAfter === "") {
+    throw new Error("NotOnOrAfter attribute must be a non empty string");
+  }
+  const notOnOrAfterDate = utcStringToDate(notOnOrAfter, "NotOnOrAfter");
+  if (notOnOrAfterDate.getTime() < Date.now()) {
+    throw new Error("NotOnOrAfter must be in the future");
+  }
+};
+
+const assertionValidation = (
+  Assertion: Element,
+  samlConfig: SamlConfig,
+  InResponseTo: string
+) => {
+  const Subject = Assertion.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "Subject"
+  ).item(0);
+  if (!Subject || isEmptyNode(Subject)) {
+    throw new Error("A non empty Subject element must be present");
+  }
+  const SubjectNameID = Subject.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "NameID"
+  ).item(0);
+  if (!SubjectNameID || isEmptyNode(SubjectNameID)) {
+    throw new Error("A non empty NameID element must be present");
+  }
+  const FormatNameID = SubjectNameID.getAttribute("Format");
+  if (!FormatNameID || FormatNameID === "") {
+    throw new Error(
+      "Format attribute of NameID element must be a non empty string"
+    );
+  }
+  if (FormatNameID !== "urn:oasis:names:tc:SAML:2.0:nameid-format:transient") {
+    throw new Error("Format attribute of NameID element is invalid");
+  }
+  const NameQualifierNameID = SubjectNameID.getAttribute("NameQualifier");
+  if (!NameQualifierNameID || NameQualifierNameID === "") {
+    throw new Error(
+      "NameQualifier attribute of NameID element must be a non empty string"
+    );
+  }
+  const SubjectConfirmation = Subject.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "SubjectConfirmation"
+  ).item(0);
+  if (!SubjectConfirmation || isEmptyNode(SubjectConfirmation)) {
+    throw new Error("SubjectConfirmation element must be present");
+  }
+  const MethodSubjectConfirmation = SubjectConfirmation.getAttribute("Method");
+  if (!MethodSubjectConfirmation || MethodSubjectConfirmation === "") {
+    throw new Error(
+      "Method attribute of SubjectConfirmation element must be a non empty string"
+    );
+  }
+  if (MethodSubjectConfirmation !== "urn:oasis:names:tc:SAML:2.0:cm:bearer") {
+    throw new Error(
+      "Method attribute of SubjectConfirmation element is invalid"
+    );
+  }
+  const SubjectConfirmationData = SubjectConfirmation.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "SubjectConfirmationData"
+  ).item(0);
+  if (!SubjectConfirmationData) {
+    throw new Error("SubjectConfirmationData element must be provided");
+  }
+  const RecipientSubjectConfirmationData = SubjectConfirmationData.getAttribute(
+    "Recipient"
+  );
+  if (
+    !RecipientSubjectConfirmationData ||
+    RecipientSubjectConfirmationData === ""
+  ) {
+    throw new Error(
+      "Recipient attribute of SubjectConfirmationData element must be a non empty string"
+    );
+  }
+  if (RecipientSubjectConfirmationData !== samlConfig.callbackUrl) {
+    throw new Error(
+      "Recipient attribute of SubjectConfirmationData element must be equal to AssertionConsumerServiceURL"
+    );
+  }
+  const InReponseToSubjectConfirmationData = SubjectConfirmationData.getAttribute(
+    "InResponseTo"
+  );
+  if (
+    !InReponseToSubjectConfirmationData ||
+    InReponseToSubjectConfirmationData === ""
+  ) {
+    throw new Error(
+      "InResponseTo attribute of SubjectConfirmationData element must be a non empty string"
+    );
+  }
+  if (InReponseToSubjectConfirmationData !== InResponseTo) {
+    throw new Error(
+      "InResponseTo attribute of SubjectConfirmationData element must be equal to Response InResponseTo"
+    );
+  }
+  notOnOrAfterValidation(SubjectConfirmationData);
+  validateIssuer(Assertion);
+  const Conditions = Assertion.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "Conditions"
+  ).item(0);
+  if (!Conditions || isEmptyNode(Conditions)) {
+    throw new Error("Conditions element must be provided");
+  }
+  const NotBeforeConditions = Conditions.getAttribute("NotBefore");
+  if (!NotBeforeConditions || NotBeforeConditions === "") {
+    throw new Error("NotBefore must be a non empty string");
+  }
+  const NotBeforeConditionsValue = utcStringToDate(
+    NotBeforeConditions,
+    "NotBefore"
+  );
+  if (NotBeforeConditionsValue.getTime() > Date.now()) {
+    throw new Error("NotBefore must be in the past");
+  }
+  notOnOrAfterValidation(Conditions);
+  const AudienceRestriction = Conditions.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "AudienceRestriction"
+  ).item(0);
+  if (!AudienceRestriction || isEmptyNode(AudienceRestriction)) {
+    throw new Error(
+      "AudienceRestriction element must be present and not empty"
+    );
+  }
+  const Audience = AudienceRestriction.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "Audience"
+  ).item(0);
+  if (!Audience || Audience.textContent !== samlConfig.issuer) {
+    throw new Error("Audience missing or invalid");
+  }
+  const AuthnStatement = Assertion.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "AuthnStatement"
+  ).item(0);
+  if (!AuthnStatement || isEmptyNode(AuthnStatement)) {
+    throw new Error("Missing or empty AuthnStatement");
+  }
+  const AuthnContext = AuthnStatement.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "AuthnContext"
+  ).item(0);
+
+  if (!AuthnContext || isEmptyNode(AuthnContext)) {
+    throw new Error("Missing or empty AuthnContext");
+  }
+
+  const AuthnContextClassRef = AuthnContext.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "AuthnContextClassRef"
+  ).item(0);
+  if (!AuthnContextClassRef || isEmptyNode(AuthnContextClassRef)) {
+    throw new Error("AuthnContextClassRef must be a non empty string");
+  }
+  // TODO: Check AuthnContextClassRef with reqest protocol value (94)(95)(96)
+  if (
+    AuthnContextClassRef.textContent !== SPID_LEVELS.SpidL1 &&
+    AuthnContextClassRef.textContent !== SPID_LEVELS.SpidL2 &&
+    AuthnContextClassRef.textContent !== SPID_LEVELS.SpidL3
+  ) {
+    throw new Error("Invalid AuthnContextClassRef value");
+  }
+  const AttributeStatement = Assertion.getElementsByTagNameNS(
+    SAML_NAMESPACE.ASSERTION,
+    "AttributeStatement"
+  ).item(0);
+  if (AttributeStatement) {
+    const Attributes = AttributeStatement.getElementsByTagNameNS(
+      SAML_NAMESPACE.ASSERTION,
+      "Attribute"
+    );
+    if (Attributes.length === 0 || Array.from(Attributes).some(isEmptyNode)) {
+      throw new Error("Attribute element must be present and not empty");
+    }
+    // TODO: Attribute into the response different from the Attribute into the request (103)
+  }
+};
+
 export const preValidateResponse: PreValidateResponseT = (
   samlConfig,
   body,
@@ -487,39 +753,25 @@ export const preValidateResponse: PreValidateResponseT = (
       .item(0);
 
     if (Response) {
-      const ID = Response.getAttribute("ID");
-      if (!ID || ID === "") {
-        throw new Error("Response must contain a non empty ID");
+      const IssueInstantValue = mainAttributeValidation(Response);
+      const InResponseTo = Response.getAttribute("InResponseTo");
+      if (!InResponseTo || InResponseTo === "") {
+        throw new Error("InResponseTo must contain a non empty string");
       }
-      const Version = Response.getAttribute("Version");
-      if (Version !== "2.0") {
-        throw new Error("Response version must be 2.0");
-      }
-      const IssueInstant = Response.getAttribute("IssueInstant");
-      if (!IssueInstant) {
-        throw new Error("Response must contain a non empty IssueInstant");
-      }
-      const IssueInstantValue =
-        IssueInstant.length === 24
-          ? UTCISODateFromString.decode(IssueInstant)
-          : UTCISODateFromString.decode(IssueInstant.replace("Z", ".000Z"));
-      if (isLeft(IssueInstantValue)) {
-        throw new Error("IssueInstant must be an UTCISO format date string");
-      }
-      if (IssueInstantValue.value.getTime() > Date.now()) {
-        throw new Error("IssueInstant must be in the past");
-      }
-      // TODO: Must validate that the IssueInstant of the Request must be after IssueInstant of the Request (15)
       const Destination = Response.getAttribute("Destination");
       if (!Destination || Destination === "") {
         throw new Error("Response must contain a non empty Destination");
       }
-      // TODO: Destination must be equal to AssertionConsumerServiceURL (21)
+      if (Destination !== samlConfig.callbackUrl) {
+        throw new Error(
+          "Destination must be equalt to AssertionConsumerServiceURL"
+        );
+      }
       const StatusElement = Response.getElementsByTagNameNS(
         SAML_NAMESPACE.PROTOCOL,
         "Status"
       ).item(0);
-      if (!StatusElement || !StatusElement.hasChildNodes()) {
+      if (!StatusElement || isEmptyNode(StatusElement)) {
         throw new Error("Status element must be present");
       }
       const StatusCodeElement = StatusElement.getElementsByTagNameNS(
@@ -536,37 +788,40 @@ export const preValidateResponse: PreValidateResponseT = (
       if (StatusCodeValue !== "urn:oasis:names:tc:SAML:2.0:status:Success") {
         throw new Error("Value attribute of StatusCode is invalid"); // TODO: Must be shown an error page to the user (26)
       }
-      const IssuerElement = Response.getElementsByTagNameNS(
-        SAML_NAMESPACE.ASSERTION,
-        "Issuer"
-      ).item(0);
-      if (
-        !IssuerElement ||
-        !IssuerElement.textContent ||
-        IssuerElement.textContent === ""
-      ) {
-        throw new Error("Issuer element must be present and not empty");
-      }
-      // TODO: Must validate that Issuer value is equal to EntityID IDP (29)
-      const IssuerFormatValue = IssuerElement.getAttribute("Format");
-      if (!IssuerFormatValue) {
-        throw new Error("Format attribute of Issuer element must be present");
-      }
-      if (
-        IssuerFormatValue !== "urn:oasis:names:tc:SAML:2.0:nameid-format:entity"
-      ) {
-        throw new Error("Format attribute of Issuer element is invalid");
-      }
-      const AssertionElement = Response.getElementsByTagNameNS(
+      validateIssuer(Response);
+      const Assertion = Response.getElementsByTagNameNS(
         SAML_NAMESPACE.ASSERTION,
         "Assertion"
       ).item(0);
-      if (!AssertionElement) {
+      if (!Assertion) {
         throw new Error("Assertion element must be present");
       }
+      const AssertionIssueInstantValue = mainAttributeValidation(Assertion);
+      assertionValidation(Assertion, samlConfig, InResponseTo);
+      samlConfig.cacheProvider?.get(InResponseTo, (err, value) => {
+        try {
+          if (err) {
+            throw new Error("Error reading the cache provider");
+          }
+          const RequestIssueInstant = new Date(value);
+          if (IssueInstantValue.getTime() < RequestIssueInstant.getTime()) {
+            throw new Error(
+              "Request IssueInstant must after Request IssueInstant"
+            );
+          }
+          if (
+            AssertionIssueInstantValue.getTime() < RequestIssueInstant.getTime()
+          ) {
+            throw new Error(
+              "Assertion IssueInstant must after Request IssueInstant"
+            );
+          }
+          callback(null, true);
+        } catch (e) {
+          callback(e);
+        }
+      });
     }
-
-    callback(null, true);
   } catch (e) {
     logger.error("Error parsing IDP response:%s", e.message);
     callback(e);
