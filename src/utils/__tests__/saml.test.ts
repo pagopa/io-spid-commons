c0b6f032b08297e059e876a89e49e90103f7d26c
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2020-03-17 11:53:51 +0100

    [#171800318] preValidate with acceptedClockSkewMs (#29)

--- src/utils/__tests__/saml.test.ts
+++ src/utils/__tests__/saml.test.ts
@@ -3,7 +3,11 @@ import { isSome, tryCatch } from "fp-ts/lib/Option";
 import { fromEither } from "fp-ts/lib/TaskEither";
 import { SamlConfig } from "passport-saml";
 import { DOMParser } from "xmldom";
-import { samlRequest, samlResponse, samlResponseCIE } from "../__mocks__/saml";
+import {
+  getSamlResponse,
+  samlRequest,
+  samlResponseCIE
+} from "../__mocks__/saml";
 import { StrictResponseValidationOptions } from "../middleware";
 import { getPreValidateResponse, getXmlFromSamlResponse } from "../saml";
 import * as saml from "../saml";
@@ -39,17 +43,32 @@ describe("preValidateResponse", () => {
     remove: jest.fn(),
     save: jest.fn()
   };
+  const mockBody = "MOCKED BODY";
+  const mockTestIdpIssuer = "http://localhost:8080";
 
-  const asyncExpectOnCallback = (callback: jest.Mock) =>
+  const expectedDesynResponseValueMs = 2000;
+
+  const asyncExpectOnCallback = (callback: jest.Mock, error?: Error) =>
     new Promise(resolve => {
       setTimeout(() => {
-        expect(callback).toBeCalledWith(null, true, expect.any(String));
+        error
+          ? expect(callback).toBeCalledWith(error)
+          : expect(callback).toBeCalledWith(null, true, expect.any(String));
         resolve();
       }, 100);
     });
 
   beforeEach(() => {
     jest.resetAllMocks();
+    mockGet.mockImplementation(() => {
+      return fromEither(
+        right({
+          RequestXML: samlRequest,
+          createdAt: "2020-02-26T07:27:42Z",
+          idpIssuer: mockTestIdpIssuer
+        })
+      );
+    });
   });
 
   afterAll(() => {
@@ -57,24 +76,14 @@ describe("preValidateResponse", () => {
   });
 
   it("should preValidate succeded with a valid saml Response", async () => {
-    const mockBody = "MOCKED BODY";
     mockGetXmlFromSamlResponse.mockImplementation(() =>
-      tryCatch(() => new DOMParser().parseFromString(samlResponse))
+      tryCatch(() => new DOMParser().parseFromString(getSamlResponse()))
     );
-    mockGet.mockImplementation(() => {
-      return fromEither(
-        right({
-          RequestXML: samlRequest,
-          createdAt: "2020-02-26T07:27:42Z",
-          idpIssuer: "http://localhost:8080"
-        })
-      );
-    });
     const strictValidationOption: StrictResponseValidationOptions = {
-      "http://localhost:8080": true
+      mockTestIdpIssuer: true
     };
     getPreValidateResponse(strictValidationOption)(
-      samlConfig,
+      { ...samlConfig, acceptedClockSkewMs: 0 },
       mockBody,
       mockRedisCacheProvider,
       mockCallback
@@ -83,29 +92,103 @@ describe("preValidateResponse", () => {
     await asyncExpectOnCallback(mockCallback);
   });
 
-  it("should preValidate succeded with a valid CIE saml Response", async () => {
-    const mockBody = "MOCKED BODY";
+  it("should preValidate succeed if timers are desynchronized and acceptedClockSkewMs is disabled", async () => {
     mockGetXmlFromSamlResponse.mockImplementation(() =>
-      tryCatch(() => new DOMParser().parseFromString(samlResponseCIE))
+      tryCatch(() =>
+        new DOMParser().parseFromString(
+          getSamlResponse(expectedDesynResponseValueMs)
+        )
+      )
     );
-    // tslint:disable-next-line: no-identical-functions
-    mockGet.mockImplementation(() => {
-      return fromEither(
-        right({
-          RequestXML: samlRequest,
-          createdAt: "2020-02-26T07:27:42Z",
-          idpIssuer:
-            "https://idserver.servizicie.interno.gov.it:8443/idp/profile/SAML2/POST/SSO"
-        })
-      );
-    });
-    getPreValidateResponse()(
-      samlConfig,
+    const strictValidationOption: StrictResponseValidationOptions = {
+      mockTestIdpIssuer: true
+    };
+    getPreValidateResponse(strictValidationOption)(
+      { ...samlConfig, acceptedClockSkewMs: -1 },
       mockBody,
       mockRedisCacheProvider,
       mockCallback
     );
     expect(mockGetXmlFromSamlResponse).toBeCalledWith(mockBody);
     await asyncExpectOnCallback(mockCallback);
+  });
+
+  it("should preValidate succeed if timers desync is less than acceptedClockSkewMs", async () => {
+    mockGetXmlFromSamlResponse.mockImplementation(() =>
+      tryCatch(() =>
+        new DOMParser().parseFromString(
+          getSamlResponse(expectedDesynResponseValueMs)
+        )
+      )
+    );
+    const strictValidationOption: StrictResponseValidationOptions = {
+      mockTestIdpIssuer: true
+    };
+    getPreValidateResponse(strictValidationOption)(
+      { ...samlConfig, acceptedClockSkewMs: 2000 },
+      mockBody,
+      mockRedisCacheProvider,
+      mockCallback
+    );
+    expect(mockGetXmlFromSamlResponse).toBeCalledWith(mockBody);
+    await asyncExpectOnCallback(mockCallback);
+  });
+
+  it("should preValidate fail if timer desync exceeds acceptedClockSkewMs", async () => {
+    mockGetXmlFromSamlResponse.mockImplementation(() =>
+      tryCatch(() =>
+        new DOMParser().parseFromString(
+          getSamlResponse(expectedDesynResponseValueMs)
+        )
+      )
+    );
+    const strictValidationOption: StrictResponseValidationOptions = {
+      mockTestIdpIssuer: true
+    };
+    getPreValidateResponse(strictValidationOption)(
+      {
+        ...samlConfig,
+        acceptedClockSkewMs: expectedDesynResponseValueMs - 100
+      },
+      mockBody,
+      mockRedisCacheProvider,
+      mockCallback
+    );
+    expect(mockGetXmlFromSamlResponse).toBeCalledWith(mockBody);
+    await asyncExpectOnCallback(
+      mockCallback,
+      new Error("IssueInstant must be in the past")
+    );
+  });
+
+  describe("preValidateResponse with CIE saml Response", () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it("should preValidate succeded with a valid CIE saml Response", async () => {
+      mockGetXmlFromSamlResponse.mockImplementation(() =>
+        tryCatch(() => new DOMParser().parseFromString(samlResponseCIE))
+      );
+      // tslint:disable-next-line: no-identical-functions
+      mockGet.mockImplementation(() => {
+        return fromEither(
+          right({
+            RequestXML: samlRequest,
+            createdAt: "2020-02-26T07:27:42Z",
+            idpIssuer:
+              "https://idserver.servizicie.interno.gov.it:8443/idp/profile/SAML2/POST/SSO"
+          })
+        );
+      });
+      getPreValidateResponse()(
+        samlConfig,
+        mockBody,
+        mockRedisCacheProvider,
+        mockCallback
+      );
+      expect(mockGetXmlFromSamlResponse).toBeCalledWith(mockBody);
+      await asyncExpectOnCallback(mockCallback);
+    });
   });
 });
