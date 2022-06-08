e0952fc0dbda10951581956c78d7e7039ec04b70
arcogabbo <arcogabbo@gmail.com>
2022-06-08 12:18:15 +0200

    applied suggestions on redis_cache_provider.ts

--- src/strategy/redis_cache_provider.ts
+++ src/strategy/redis_cache_provider.ts
@@ -1,8 +1,5 @@
-// eslint-disable-next-line import/no-internal-modules
 import { UTCISODateFromString } from "@pagopa/ts-commons/lib/dates";
-// eslint-disable-next-line import/no-internal-modules
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
-// eslint-disable-next-line import/no-internal-modules
 import { Second } from "@pagopa/ts-commons/lib/units";
 import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
@@ -36,26 +33,25 @@ export interface IExtendedCacheProvider {
 // practically no error handling in passport-saml
 // (a very bad lot of spaghetti code)
 export const noopCacheProvider = (): CacheProvider => ({
+  // invokes 'callback' and passes the value if found, null otherwise
+  get: (_, callback): void => {
+    callback(null, {});
+  },
+
+  // removes the key from the cache, invokes `callback` with the
+  // key removed, null if no key is removed
+  remove: (key, callback): void => {
+    callback(null, key);
+  },
+
   // saves the key with the optional value
   // invokes the callback with the value saved
-  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-  save(_, value, callback): void {
+  save: (_, value, callback): void => {
     const v = {
       createdAt: new Date(),
       value
     };
     callback(null, v);
-  },
-  // invokes 'callback' and passes the value if found, null otherwise
-  // eslint-disable-next-line sort-keys, prefer-arrow/prefer-arrow-functions
-  get(_, callback): void {
-    callback(null, {});
-  },
-  // removes the key from the cache, invokes `callback` with the
-  // key removed, null if no key is removed
-  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-  remove(key, callback): void {
-    callback(null, key);
   }
 });
 
@@ -65,12 +61,58 @@ export const getExtendedRedisCacheProvider = (
   keyExpirationPeriodSeconds: Second = 3600 as Second,
   keyPrefix: string = "SAML-EXT-"
 ): IExtendedCacheProvider => ({
-  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-  save(
+  get: (AuthnRequestID: string): TaskEither<Error, SAMLRequestCacheItem> =>
+    pipe(
+      TE.taskify(
+        (key: string, callback: (e: Error | null, value?: string) => void) => {
+          redisClient.get(key, (e, v) =>
+            // redis callbacks consider empty value as null instead of undefined,
+            //  hence the need for the following wrapper to convert nulls to undefined
+            callback(e, v === null ? undefined : v)
+          );
+        }
+      )(`${keyPrefix}${AuthnRequestID}`),
+      TE.mapLeft(
+        err => new Error(`SAML#ExtendedRedisCacheProvider: get() error ${err}`)
+      ),
+      TE.chain(value =>
+        TE.fromEither(
+          pipe(
+            E.parseJSON(value, E.toError),
+            E.chain(_ =>
+              pipe(
+                SAMLRequestCacheItem.decode(_),
+                E.mapLeft(
+                  __ =>
+                    new Error(
+                      `SAML#ExtendedRedisCacheProvider: get() error ${readableReport(
+                        __
+                      )}`
+                    )
+                )
+              )
+            )
+          )
+        )
+      )
+    ),
+  remove: (AuthnRequestID): TaskEither<Error, string> =>
+    pipe(
+      TE.taskify(
+        (key: string, callback: (err: Error | null, value?: unknown) => void) =>
+          redisClient.del(key, callback)
+      )(`${keyPrefix}${AuthnRequestID}`),
+      TE.mapLeft(
+        err =>
+          new Error(`SAML#ExtendedRedisCacheProvider: remove() error ${err}`)
+      ),
+      TE.map(() => AuthnRequestID)
+    ),
+  save: (
     RequestXML: string,
     samlConfig: SamlConfig
-  ): TaskEither<Error, SAMLRequestCacheItem> {
-    return pipe(
+  ): TaskEither<Error, SAMLRequestCacheItem> =>
+    pipe(
       TE.fromEither(
         E.fromOption(
           () =>
@@ -118,57 +160,5 @@ export const getExtendedRedisCacheProvider = (
           TE.map(() => v)
         );
       })
-    );
-  },
-  // eslint-disable-next-line sort-keys, prefer-arrow/prefer-arrow-functions
-  get(AuthnRequestID: string): TaskEither<Error, SAMLRequestCacheItem> {
-    return pipe(
-      TE.taskify(
-        (key: string, callback: (e: Error | null, value?: string) => void) => {
-          redisClient.get(key, (e, v) =>
-            // redis callbacks consider empty value as null instead of undefined,
-            //  hence the need for the following wrapper to convert nulls to undefined
-            callback(e, v === null ? undefined : v)
-          );
-        }
-      )(`${keyPrefix}${AuthnRequestID}`),
-      TE.mapLeft(
-        err => new Error(`SAML#ExtendedRedisCacheProvider: get() error ${err}`)
-      ),
-      TE.chain(value =>
-        TE.fromEither(
-          pipe(
-            E.parseJSON(value, E.toError),
-            E.chain(_ =>
-              pipe(
-                SAMLRequestCacheItem.decode(_),
-                E.mapLeft(
-                  __ =>
-                    new Error(
-                      `SAML#ExtendedRedisCacheProvider: get() error ${readableReport(
-                        __
-                      )}`
-                    )
-                )
-              )
-            )
-          )
-        )
-      )
-    );
-  },
-  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-  remove(AuthnRequestID): TaskEither<Error, string> {
-    return pipe(
-      TE.taskify(
-        (key: string, callback: (err: Error | null, value?: unknown) => void) =>
-          redisClient.del(key, callback)
-      )(`${keyPrefix}${AuthnRequestID}`),
-      TE.mapLeft(
-        err =>
-          new Error(`SAML#ExtendedRedisCacheProvider: remove() error ${err}`)
-      ),
-      TE.map(() => AuthnRequestID)
-    );
-  }
+    )
 });
