90842ab4d10c6f7449886a4c130fca2e3fb944db
Daniele Manni <dan.manni@gmail.com>
2023-07-13 17:35:53 +0200

    [#IOPID-437] Add integrity unit test

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -1,4 +1,10 @@
-import { IDP_NAMES } from "../config";
+import { pipe } from "fp-ts/lib/function";
+import {
+  CIE_IDP_IDENTIFIERS,
+  IDP_NAMES,
+  SPID_IDP_IDENTIFIERS,
+} from "../config";
+import * as RA from "fp-ts/lib/ReadonlyArray";
 describe("IDP_NAMES", () => {
   it("should return the IdP name if a valid entityId is provided", () => {
     const aValidEntityID = "https://id.eht.eu";
@@ -10,5 +16,17 @@ describe("IDP_NAMES", () => {
     const aInvalidEntityID = "https://invalid.entity.id";
     const idpName = IDP_NAMES[aInvalidEntityID];
     expect(idpName).toBeUndefined();
+  });
+
+  it("All the possible entityId should be mapped as IdP names", () => {
+    const possibleEntityID = [
+      ...Object.keys(SPID_IDP_IDENTIFIERS),
+      ...Object.keys(CIE_IDP_IDENTIFIERS),
+    ];
+    pipe(
+      possibleEntityID,
+      RA.map((entityId) => IDP_NAMES[entityId]),
+      RA.map((entityName) => expect(entityName).toBeDefined())
+    );
   });
 });
